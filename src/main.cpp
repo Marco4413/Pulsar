2c941e2bab5b0bcb626ae22b3ed39d6f299970b8
Marco <46133335+Marco4413@users.noreply.github.com>
2024-02-15 15:27:00 +0100

    Pulsar-Dev: Pretty runtime error printing.

--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,54 @@ std::ostream& operator<<(std::ostream& ostream, const Pulsar::Value& val)
     return ostream << val.AsInteger;
 }
 
-void PrintPrettyError(const std::string& source, const char* filepath, const Pulsar::Token& token, const char* message)
+void PrintTokenView(const std::string& source, const Pulsar::Token& token)
 {
-    std::printf("%s:%lu:%lu: Error: %s\n", filepath, token.SourcePos.Line+1, token.SourcePos.Char, message);
-
     Pulsar::Structures::StringView errorView(source);
     errorView.RemovePrefix(token.SourcePos.Index-token.SourcePos.Char+1);
     size_t lineChars = 0;
     for (; lineChars < errorView.Length() && errorView[lineChars] != '\r' && errorView[lineChars] != '\n'; lineChars++);
     std::printf("%.*s\n", (int)lineChars, errorView.CStringFrom(0));
-    
+}
+
+void PrintPrettyError(const std::string& source, const char* filepath, const Pulsar::Token& token, const std::string& message)
+{
+    std::printf("%s:%lu:%lu: Error: %s\n", filepath, token.SourcePos.Line+1, token.SourcePos.Char, message.c_str());
+    PrintTokenView(source, token);
     const std::string tokenUnderline(token.SourcePos.CharSpan-1, '~');
     if (token.SourcePos.Char > 0)
         std::printf("%*s", (int)token.SourcePos.Char-1, "");
     std::printf("^%s\n", tokenUnderline.c_str());
+}
+
+void PrintPrettyRuntimeError(const std::string& source, const char* filepath, const Pulsar::ExecutionContext& context)
+{
+    if (context.CallStack.size() == 0) {
+        std::cout << "No Runtime Error Information." << std::endl;
+        return;
+    }
+
+    const Pulsar::Frame& frame = context.GetCurrentFrame();
+    if (!frame.Function.HasDebugSymbol()) {
+        std::cout << "Error: Within function " << frame.Function.Name << std::endl;
+        std::cout << "    No Code Debug Symbols found." << std::endl;
+        return;
+    } else if (!frame.Function.HasCodeDebugSymbols()) {
+        PrintPrettyError(
+            source, filepath,
+            frame.Function.FunctionDebugSymbol.Token,
+            "Within function " + frame.Function.Name);
+        return;
+    }
+
+    size_t symbolIdx = 0;
+    for (size_t i = 0; i < frame.Function.CodeDebugSymbols.size(); i++) {
+        // frame.InstructionIndex points to the NEXT instruction
+        if (frame.Function.CodeDebugSymbols[i].StartIdx >= frame.InstructionIndex)
+            break;
+        symbolIdx = i;
+    }
+
+    PrintPrettyError(source, filepath, frame.Function.CodeDebugSymbols[symbolIdx].Token, "In function " + frame.Function.Name);
 }
 
 int main(int argc, const char** argv)
@@ -54,7 +88,7 @@ int main(int argc, const char** argv)
 
     Pulsar::Module module;
     Pulsar::Parser parser(source);
-    auto result = parser.ParseIntoModule(module);
+    auto result = parser.ParseIntoModule(module, true);
     if (result != Pulsar::ParseResult::OK) {
         PrintPrettyError(parser.GetSource(), program, parser.GetLastErrorToken(), parser.GetLastErrorMessage());
         std::cout << "Parse Error: " << (int)result << std::endl;
@@ -79,10 +113,14 @@ int main(int argc, const char** argv)
         });
 
     Pulsar::Stack stack;
-    auto runtimeState = module.CallFunctionByName("main", stack);
+    Pulsar::ExecutionContext context = module.CreateExecutionContext();
+    auto runtimeState = module.CallFunctionByName("main", stack, context);
+
     std::cout << "Runtime State: " << Pulsar::RuntimeStateToString(runtimeState) << std::endl;
-    if (runtimeState != Pulsar::RuntimeState::OK)
+    if (runtimeState != Pulsar::RuntimeState::OK) {
+        PrintPrettyRuntimeError(parser.GetSource(), program, context);
         return 1;
+    }
 
     std::cout << "Stack Dump:" << std::endl;
     for (size_t i = 0; i < stack.size(); i++)
