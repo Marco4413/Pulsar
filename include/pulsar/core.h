869b4f7de004f4ff8acfc45cece4387a986c337b
Marco <46133335+Marco4413@users.noreply.github.com>
2024-02-22 13:54:52 +0100

    Core: Added malloc macro and custom new and delete.
    Everything is now done through PULSAR_MALLOC/FREE

--- include/pulsar/core.h
+++ include/pulsar/core.h
@@ -9,6 +9,10 @@
 #include <unordered_map>
 #include <vector>
 
+#ifndef PULSAR_MALLOC
+#define PULSAR_MALLOC std::malloc
+#endif // PULSAR_MALLOC
+
 #ifndef PULSAR_REALLOC
 #define PULSAR_REALLOC std::realloc
 #endif // PULSAR_REALLOC
@@ -25,12 +29,32 @@
 #define PULSAR_MEMCPY std::memcpy
 #endif // PULSAR_MEMCPY
 
+namespace Pulsar::Core
+{
+    // New and Delete that strictly use PULSAR_ macros to allocate memory
+    template<typename T, typename ...Args>
+    inline T* New(Args ...args)
+    {
+        T* ptr = (T*)PULSAR_MALLOC(sizeof(T));
+        return new(ptr) T(args...);
+    }
+
+    template<typename T>
+    inline void Delete(T* ptr)
+    {
+        if (ptr) {
+            ptr->~T();
+            PULSAR_FREE(ptr);
+        }
+    }
+}
+
 #ifndef PULSAR_NEW
-#define PULSAR_NEW(T, ...) new T(__VA_ARGS__)
+#define PULSAR_NEW(T, ...) Pulsar::Core::New<T>(__VA_ARGS__)
 #endif // PULSAR_NEW
 
 #ifndef PULSAR_DELETE
-#define PULSAR_DELETE(T, ptr) delete (ptr)
+#define PULSAR_DELETE(T, ptr) Pulsar::Core::Delete<T>(ptr)
 #endif // PULSAR_DELETE
 
 #endif // _PULSAR_CORE_H
