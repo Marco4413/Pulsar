dc627fc02a641f3be6c05187aa88a3e535595fa3
Marco <46133335+Marco4413@users.noreply.github.com>
2024-03-30 23:48:30 +0100

    Runtime: Set InstructionCode(s) to fixed values.

--- include/pulsar/runtime/instruction.h
+++ include/pulsar/runtime/instruction.h
@@ -6,52 +6,86 @@
 namespace Pulsar
 {
     enum class InstructionCode {
-        // Stack and Locals
-        PushInt, PushDbl,
-        PushFunctionReference,
-        PushNativeFunctionReference,
-        PushConst,
-        PushLocal, MoveLocal,
-        PopIntoLocal, CopyIntoLocal,
-        PushGlobal, MoveGlobal,
-        PopIntoGlobal, CopyIntoGlobal,
-        Pop, Swap, Dup,
+        // Stack
+        PushInt = 0x01,
+        PushDbl = 0x02,
+        PushFunctionReference       = 0x03,
+        PushNativeFunctionReference = 0x04,
+        PushEmptyList = 0x05,
+
+        Pop  = 0x0D,
+        Swap = 0x0E,
+        Dup  = 0x0F,
+
+        // Constants, Locals and Globals
+        PushConst = 0x10,
+
+        PushLocal     = 0x11,
+        MoveLocal     = 0x12,
+        PopIntoLocal  = 0x13,
+        CopyIntoLocal = 0x14,
+
+        PushGlobal     = 0x15,
+        MoveGlobal     = 0x16,
+        PopIntoGlobal  = 0x17,
+        CopyIntoGlobal = 0x18,
+
         // Functions
-        Call, CallNative, Return,
-        ICall,
+        Return     = 0x20,
+        Call       = 0x21,
+        CallNative = 0x22,
+        ICall      = 0x2F,
+
         // Math
-        DynSum, DynSub, DynMul, DynDiv,
-        Mod,
-        BitAnd, BitOr, BitNot, BitXor,
-        BitShiftLeft, BitShiftRight,
-        Floor, Ceil,
-        // Conditional Jumps
-        Compare,
-        Jump,
-        JumpIfZero,
-        JumpIfNotZero,
-        JumpIfGreaterThanZero,
-        JumpIfGreaterThanOrEqualToZero,
-        JumpIfLessThanZero,
-        JumpIfLessThanOrEqualToZero,
-        // Lists and Strings
-        Length, IsEmpty,
-        PushEmptyList,
-        Prepend, Append, Concat,
-        Head, Tail,
-        Index,
-        Prefix, Suffix, Substr,
+        DynSum = 0x30,
+        DynSub = 0x31,
+        DynMul = 0x32,
+        DynDiv = 0x33,
+        Mod    = 0x34,
+        BitAnd = 0x38,
+        BitOr  = 0x39,
+        BitNot = 0x3A,
+        BitXor = 0x3B,
+        BitShiftLeft  = 0x3C,
+        BitShiftRight = 0x3D,
+        Floor = 0x40,
+        Ceil  = 0x41,
+        // Comparison
+        Compare = 0x50,
+        // Jumps
+        Jump                           = 0x60,
+        JumpIfZero                     = 0x61,
+        JumpIfNotZero                  = 0x62,
+        JumpIfGreaterThanZero          = 0x63,
+        JumpIfGreaterThanOrEqualToZero = 0x64,
+        JumpIfLessThanZero             = 0x65,
+        JumpIfLessThanOrEqualToZero    = 0x66,
+        // Sized Values
+        IsEmpty = 0x70,
+        Length  = 0x71,
+        Prepend = 0x72,
+        Append  = 0x73,
+        Index   = 0x74,
+        //   List-Specific
+        Concat = 0x78,
+        Head   = 0x79,
+        Tail   = 0x7A,
+        //   String-Specific
+        Prefix = 0x7C,
+        Suffix = 0x7D,
+        Substr = 0x7E,
         // Type Checking
-        IsVoid,
-        IsInteger,
-        IsDouble,
-        IsNumber,
-        IsFunctionReference,
-        IsNativeFunctionReference,
-        IsAnyFunctionReference,
-        IsList,
-        IsString,
-        IsCustom,
+        IsVoid    = 0x80,
+        IsInteger = 0x81,
+        IsDouble  = 0x82,
+        IsFunctionReference       = 0x83,
+        IsNativeFunctionReference = 0x84,
+        IsList   = 0x85,
+        IsString = 0x86,
+        IsCustom = 0x8F,
+        // Compound Type Checking
+        IsNumber = 0x90,
+        IsAnyFunctionReference = 0x91,
     };
 
     struct Instruction
