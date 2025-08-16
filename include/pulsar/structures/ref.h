e604a23d1404647e8c53bfa0a3169545f7c33a9e
Marco <46133335+Marco4413@users.noreply.github.com>
2025-08-16 02:09:57 +0200

    Structures: Refs now hold ref counts and data close in memory.

--- include/pulsar/structures/ref.h
+++ include/pulsar/structures/ref.h
@@ -5,13 +5,19 @@
 
 namespace Pulsar
 {
-    namespace Ref
+    struct RefCount
     {
-        struct RefCount
-        {
-            PULSAR_ATOMIC_SIZE_T SharedRefs = 0;
-        };
-    }
+        PULSAR_ATOMIC_SIZE_T SharedRefs = 0;
+    };
+
+    template <typename T>
+    struct RefCountedValue
+    {
+        // This field MUST always come first so that calling free
+        //  on a malloc'd block frees the whole struct
+        Pulsar::RefCount RefCount;
+        T Value;
+    };
 
     template <typename T>
     class SharedRef
@@ -23,18 +29,31 @@ namespace Pulsar
         template<typename ...Args>
         static SharedRef<T> New(Args&& ...args)
         {
-            SharedRef<T> ref(PULSAR_NEW(T, std::forward<Args>(args)...));
+            // Using PULSAR_MALLOC to make sure that PULSAR_FREE can be called on the block
+            auto storage = static_cast<RefCountedValue<T>*>(PULSAR_MALLOC(sizeof(RefCountedValue<T>)));
+            PULSAR_PLACEMENT_NEW(RefCount, &storage->RefCount, (size_t)0);
+            PULSAR_PLACEMENT_NEW(T, &storage->Value, std::forward<Args>(args)...);
+            SharedRef<T> ref(&storage->RefCount, &storage->Value);
             return ref;
         }
 
         SharedRef()
-            : m_Ptr(nullptr), m_RefCount(nullptr) { }
+            : m_RefCount(nullptr), m_Value(nullptr) { }
 
         SharedRef(std::nullptr_t null)
             : SharedRef() { (void)null; }
         
-        SharedRef(const SharedRef<T>& o) { *this = o; };
-        SharedRef(SharedRef<T>&& o) { *this = std::move(o); };
+        SharedRef(const SharedRef<T>& o)
+            : SharedRef()
+        {
+            *this = o;
+        }
+
+        SharedRef(SharedRef<T>&& o)
+            : SharedRef()
+        {
+            *this = std::move(o);
+        }
 
         ~SharedRef()
         {
@@ -45,38 +64,38 @@ namespace Pulsar
         {
             o.IncrementRefCount();
             this->DecrementRefCount();
-            m_Ptr = o.m_Ptr;
+            m_Value = o.m_Value;
             m_RefCount = o.m_RefCount;
             return *this;
         }
 
         SharedRef<T>& operator=(SharedRef<T>&& o)
         {
-            m_Ptr = o.m_Ptr;
+            m_Value = o.m_Value;
             m_RefCount = o.m_RefCount;
-            o.m_Ptr = nullptr;
+            o.m_Value = nullptr;
             o.m_RefCount = nullptr;
             return *this;
         }
 
-        T* operator->() const { return m_Ptr; }
+        T* operator->() const { return m_Value; }
         T& operator*() const
         {
-            PULSAR_ASSERT(Get(), "Dereferencing nullptr.");
-            return *m_Ptr;
+            PULSAR_ASSERT(m_Value, "Dereferencing nullptr.");
+            return *m_Value;
         }
 
         T& operator[](size_t idx) const
         {
-            PULSAR_ASSERT(Get(), "Indexing nullptr.");
-            return m_Ptr[idx];
+            PULSAR_ASSERT(m_Value, "Indexing nullptr.");
+            return m_Value[idx];
         }
 
         template<typename U>
-        bool operator==(const SharedRef<U>& o) const { return m_Ptr == o.m_Ptr; }
+        bool operator==(const SharedRef<U>& o) const { return m_Value == o.m_Value; }
         template<typename U>
-        bool operator!=(const SharedRef<U>& o) const { return m_Ptr != o.m_Ptr; }
-        operator bool() const { return (bool)m_Ptr; }
+        bool operator!=(const SharedRef<U>& o) const { return m_Value != o.m_Value; }
+        operator bool() const { return (bool)m_RefCount; }
 
         template<typename U>
         operator SharedRef<U>() const { return CastTo<U>(); }
@@ -85,25 +104,19 @@ namespace Pulsar
         template<typename U>
         SharedRef<U> CastTo() const
         {
-            SharedRef<U> sRef(dynamic_cast<U*>(m_Ptr), m_RefCount);
-            return sRef;
+            SharedRef<U> castedRef(m_RefCount, dynamic_cast<U*>(m_Value));
+            return castedRef;
         }
 
-        T* Get() const { return m_Ptr; }
+        T* Get() const { return m_Value; }
         size_t SharedCount() const
         {
             return m_RefCount ? (size_t)m_RefCount->SharedRefs : 1;
         }
 
     private:
-        explicit SharedRef(T* ptr)
-            : m_Ptr(ptr), m_RefCount(PULSAR_NEW(Ref::RefCount))
-        {
-            IncrementRefCount();
-        }
-
-        explicit SharedRef(T* ptr, Ref::RefCount* refs)
-            : m_Ptr(ptr), m_RefCount(refs)
+        explicit SharedRef(RefCount* refCount, T* value)
+            : m_RefCount(refCount), m_Value(value)
         {
             IncrementRefCount();
         }
@@ -117,16 +130,17 @@ namespace Pulsar
         void DecrementRefCount()
         {
             if (m_RefCount && m_RefCount->SharedRefs-- <= 1) {
-                PULSAR_DELETE(T, m_Ptr);
-                PULSAR_DELETE(Ref::RefCount, m_RefCount);
-                m_Ptr = nullptr;
+                (*m_RefCount).~RefCount();
+                (*m_Value).~T();
+                PULSAR_FREE(m_RefCount);
                 m_RefCount = nullptr;
+                m_Value = nullptr;
             }
         }
 
     private:
-        T* m_Ptr;
-        Ref::RefCount* m_RefCount;
+        RefCount* m_RefCount;
+        T* m_Value;
     };
 }
 
